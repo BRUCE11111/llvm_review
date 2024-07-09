0310f143eb2fc74d51acec4281d62495289defdb
Xu, Xiaohui1 <xiaohui1.xu@intel.com>
2024-07-09 11:27:43 +0800

    [mlir][vector] init tiling vector pass

---
+++ mlir/include/mlir/Dialect/Vector/Transforms/VectorTiling.h
@@ -0,0 +1,409 @@
+//===- VectorTiling.h - Vector Tiling Pass --*- C++------*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#ifndef MLIR_DIALECT_VECTOR_TRANSFORMS_VECTORTILING_H_
+#define MLIR_DIALECT_VECTOR_TRANSFORMS_VECTORTILING_H_
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Dialect/Tensor/Transforms/Transforms.h"
+#include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/Dialect/Vector/Transforms/Passes.h"
+#include "mlir/ExecutionEngine/Float16bits.h"
+#include "mlir/IR/AffineMap.h"
+#include "mlir/IR/BuiltinTypeInterfaces.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/IR/Visitors.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/CSE.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Transforms/LoopInvariantCodeMotionUtils.h"
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseSet.h"
+#include "llvm/ADT/SetVector.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/ErrorHandling.h"
+#include <deque>
+#include <functional>
+#include <limits>
+#include <optional>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <variant>
+namespace mlir {
+namespace vector {
+namespace {
+
+Value makeIndexArithConstantOp(OpBuilder &opBuilder, Location &loc, int64_t x);
+void setOperationCorrectOperand(
+    Operation *op, const ValueRange &iterArgs,
+    const llvm::DenseMap<Value, int> &operandIdxMap,
+    const llvm::SmallVector<Value, 5> &inductionVars,
+    const llvm::DenseMap<Operation *, AffineMap> &opPermuationMap);
+
+struct HardWareInfo {
+  bool favx512f = true;
+  bool favx2 = true;
+};
+
+class TypeHelper {
+private:
+  HardWareInfo HWInfo;
+
+public:
+  void setHardWareInfo(HardWareInfo &info) { HWInfo = info; }
+  int getDataTypeValidSteps(const VectorType &type);
+  int generateValidSteps(int steps, VectorType type);
+  int getDataTypeMAXSIMDLength(const VectorType &type);
+  VectorType getVectorzedType(Operation *op, uint32_t loopStep = 0);
+};
+
+// 1. Classify operaions:
+// classify the operations into :
+//    a. reorder, transpose. Reorder(or transpose) dim may bring data
+//    dependency.
+//    b. elemenwise. Those operations can be fused into a common for loop.
+//    c. broadcast. Need to analysis broadcast dim and the data
+//    dependency.
+//    d. reduction. Need to analysis broadcast dim and the
+//    data dependency.
+// Same group operations have no data dependencies. They can be fused into a
+// common for loop body.
+
+// Using queue to store the operation order. In order to ensure that
+// subsequent moves to the operation will not cause semantic changes.
+class VectorFusionStrategy {
+private:
+  llvm::SmallVector<std::queue<Operation *>, 8> opGroups;
+  llvm::SmallVector<uint32_t, 8> groupMaxSteps;
+  // query current operation in which group, return group index
+  llvm::DenseMap<Operation *, size_t> opGroupIndexMap;
+  // can fused into prev operation which axis position
+  llvm::DenseMap<Operation *, int32_t> opAnchorPos;
+
+  func::FuncOp func;
+
+public:
+  llvm::SmallVector<std::queue<Operation *>, 8> &getOpGroups() {
+    return opGroups;
+  }
+  llvm::DenseMap<Operation *, size_t> &getOpGroupIndexMap() {
+    return opGroupIndexMap;
+  }
+  llvm::SmallVector<uint32_t, 8> &getGroupMaxSteps() { return groupMaxSteps; }
+
+  func::FuncOp getFunc() { return func; }
+
+  VectorFusionStrategy() = default;
+  VectorFusionStrategy(func::FuncOp func) : func(func) {}
+
+  void classifyOperations();
+
+  // run the vector fusion strategy
+  void run();
+};
+
+enum CanonicalizerKind { OperationsGroup, Operations };
+
+template <class T>
+class SpecialOperationCanonicalizer {
+private:
+  llvm::SmallVector<T, 4> candidateRdOps;
+
+public:
+  SpecialOperationCanonicalizer() = default;
+  SpecialOperationCanonicalizer(const llvm::SmallVector<T, 4> &candidateRdOps)
+      : candidateRdOps(candidateRdOps) {}
+  virtual ~SpecialOperationCanonicalizer() {}
+  llvm::SmallVector<T, 4> &getCandidateOps();
+  virtual void prepareSpecialOperationInfo() = 0;
+};
+
+enum class MultiReduceOpAxisKind { Reduction, Parallel };
+class MultiReductionCanonicalizer
+    : public SpecialOperationCanonicalizer<vector::MultiDimReductionOp> {
+private:
+  llvm::SmallVector<int64_t, 4> reductionAxis, parallelAxis;
+  std::queue<Operation *> prevOps, postOps, accRelatedOps, sourceRelatedOps;
+  bool haslastDimReduction = false;
+  bool isStandaloneOp = false;
+  int64_t typeRank = -1;
+  llvm::SetVector<Value> originalOpResults;
+  VectorType sourceType, accType;
+  llvm::SmallDenseMap<Value, int> resultIdxMap;
+
+public:
+  MultiReductionCanonicalizer(
+      const llvm::SmallVector<vector::MultiDimReductionOp, 4> &candidateRdOps)
+      : SpecialOperationCanonicalizer<vector::MultiDimReductionOp>(
+            candidateRdOps) {
+    isStandaloneOp = candidateRdOps.size() == 1;
+    prepareSpecialOperationInfo();
+  };
+  virtual ~MultiReductionCanonicalizer(){};
+  int64_t getTypeRank();
+  void getReductionAxisAndParallelAxis();
+  bool hasLastDimReduction();
+  bool getIsStandAloneOp() { return isStandaloneOp; }
+  void initReductionAxis();
+  void initParallelAxis();
+  llvm::SmallVector<int64_t, 4> &getReductionAxis() { return reductionAxis; };
+  llvm::SmallVector<int64_t, 4> &getParallelAxis() { return parallelAxis; };
+  std::queue<Operation *> &getPrevOps() { return prevOps; }
+  std::queue<Operation *> &getPostOps() { return postOps; }
+  std::queue<Operation *> &getAccRelatedOps() { return accRelatedOps; }
+  std::queue<Operation *> &getSourceRelatedOps() { return sourceRelatedOps; }
+  llvm::SetVector<Value> &getOriginalOpResults() { return originalOpResults; }
+  VectorType &getSourceType() { return sourceType; };
+  VectorType &getAccType() { return accType; };
+  llvm::SmallDenseMap<Value, int> &getResultIdxMap() { return resultIdxMap; }
+  void setResultIdxMap(const llvm::SmallDenseMap<Value, int> &map) {
+    resultIdxMap = map;
+  }
+  void prepareSpecialOperationInfo() override;
+};
+
+class BroadcastCanonicalizer
+    : public SpecialOperationCanonicalizer<vector::BroadcastOp> {
+private:
+public:
+  BroadcastCanonicalizer(
+      const llvm::SmallVector<vector::BroadcastOp, 4> &candidateBcOps)
+      : SpecialOperationCanonicalizer<vector::BroadcastOp>(candidateBcOps){};
+  virtual ~BroadcastCanonicalizer() {}
+  void prepareSpecialOperationInfo() override {}
+};
+
+class TransposeCanonicalizer
+    : public SpecialOperationCanonicalizer<vector::TransposeOp> {
+private:
+public:
+  TransposeCanonicalizer(
+      const llvm::SmallVector<vector::TransposeOp, 4> &candidateTpOps)
+      : SpecialOperationCanonicalizer<vector::TransposeOp>(candidateTpOps){};
+  virtual ~TransposeCanonicalizer() {}
+  void prepareSpecialOperationInfo() override {}
+};
+
+class ShapeCastCanonicalizer
+    : public SpecialOperationCanonicalizer<vector::ShapeCastOp> {
+private:
+public:
+  ShapeCastCanonicalizer(
+      const llvm::SmallVector<vector::ShapeCastOp, 4> &candidateScOps)
+      : SpecialOperationCanonicalizer<vector::ShapeCastOp>(candidateScOps){};
+  virtual ~ShapeCastCanonicalizer() {}
+  void prepareSpecialOperationInfo() override {}
+};
+
+class CanonicalizerCommonUsedData : virtual public TypeHelper {
+private:
+  VectorFusionStrategy fusionStrategy;
+  // analysis the operation's operands and results
+  llvm::SmallVector<llvm::SetVector<Value>, 8> groupOpResults, groupOpIterArgs;
+
+  // store read and write operations permutation maps in order to convenient
+  // to replace loop induction var
+  llvm::DenseMap<Operation *, AffineMap> opPermuationMap;
+  llvm::SmallVector<MultiReductionCanonicalizer, 8> multiRdCanonicalizers;
+  llvm::SmallVector<BroadcastCanonicalizer, 8> broadcastCanonicalizers;
+  llvm::SmallVector<TransposeCanonicalizer, 8> transposeCanonicalizers;
+  llvm::SmallVector<ShapeCastCanonicalizer, 8> shapeCastCanonicalizers;
+
+public:
+  CanonicalizerCommonUsedData() = default;
+  CanonicalizerCommonUsedData(VectorFusionStrategy &fusionStrategy)
+      : fusionStrategy(fusionStrategy){};
+
+  CanonicalizerCommonUsedData(
+      VectorFusionStrategy &fusionStrategy,
+      llvm::SmallVector<llvm::SetVector<Value>, 8> &groupOpResults,
+      llvm::SmallVector<llvm::SetVector<Value>, 8> &groupOpIterArgs,
+      llvm::DenseMap<Operation *, AffineMap> &opPermuationMap)
+      : fusionStrategy(fusionStrategy), groupOpResults(groupOpResults),
+        groupOpIterArgs(groupOpIterArgs), opPermuationMap(opPermuationMap) {}
+  virtual ~CanonicalizerCommonUsedData(){};
+
+  // set methods
+  void setFuseStrategy(VectorFusionStrategy &strategy) {
+    fusionStrategy = strategy;
+    auto opGroups = fusionStrategy.getOpGroups();
+    if (opGroups.size() != groupOpResults.size() ||
+        opGroups.size() != groupOpIterArgs.size()) {
+      groupOpResults.clear();
+      groupOpIterArgs.clear();
+      for (size_t i = 0; i < opGroups.size(); i++) {
+        groupOpResults.emplace_back(llvm::SetVector<Value>());
+        groupOpIterArgs.emplace_back(llvm::SetVector<Value>());
+      }
+    }
+  }
+  void
+  setGroupOpResults(llvm::SmallVector<llvm::SetVector<Value>, 8> &results) {
+    groupOpResults = results;
+  }
+  void
+  setGroupOpIterArgs(llvm::SmallVector<llvm::SetVector<Value>, 8> &iterArgs) {
+    groupOpIterArgs = iterArgs;
+  }
+  void setPermutationMap(llvm::DenseMap<Operation *, AffineMap> &map) {
+    opPermuationMap = map;
+  }
+
+  // get methods
+  VectorFusionStrategy &getFusionStrategy() { return fusionStrategy; }
+
+  llvm::SmallVector<llvm::SetVector<Value>, 8> &getGroupOpResults() {
+    return groupOpResults;
+  }
+
+  llvm::SmallVector<llvm::SetVector<Value>, 8> &getGroupOpIterArgs() {
+    return groupOpIterArgs;
+  }
+
+  llvm::DenseMap<Operation *, AffineMap> &getOpPermuationMap() {
+    return opPermuationMap;
+  }
+
+  llvm::SmallVector<MultiReductionCanonicalizer, 8> &
+  getMultiRdCanonicalizers() {
+    return multiRdCanonicalizers;
+  }
+
+  llvm::SmallVector<BroadcastCanonicalizer, 8> &getBroadcastCanonicalizers() {
+    return broadcastCanonicalizers;
+  }
+
+  llvm::SmallVector<TransposeCanonicalizer, 8> &getTransposeCanonicalizers() {
+    return transposeCanonicalizers;
+  }
+
+  llvm::SmallVector<ShapeCastCanonicalizer, 8> &getShapeCastCanonicalizers() {
+    return shapeCastCanonicalizers;
+  }
+
+  // other methods
+  bool isGroupHasSpecialOperation(const size_t grpIdx);
+};
+
+class ForLoopGenerator : virtual public CanonicalizerCommonUsedData {
+  func::FuncOp func;
+
+public:
+  virtual ~ForLoopGenerator() {}
+  void setGeneratorFunc(func::FuncOp &func) { this->func = func; }
+  void generateGroupOpVectorizedIR(const int idx);
+  void rewriteOperationAsVectorize(OpBuilder &rewriter, size_t groupId,
+                                   const std::queue<Operation *> &queue = {});
+  void createNewConstantOp(Operation *srcOp,
+                           vector::TransferWriteOp *transferWriteOp);
+  // elementwise for loop
+  mlir::FailureOr<scf::ForOp>
+  generateVectorizedForLoop(const size_t groupId, IRRewriter &rewriter,
+                            const VectorType &vectorType);
+  scf::ForOp
+  constructNestedForOp(const size_t forDimIdx, const size_t groupIdx,
+                       OpBuilder &b, const Location &loc,
+                       const ValueRange &iterArgs, const VectorType &type,
+                       const llvm::ArrayRef<int64_t> &dims,
+                       llvm::SmallVector<Value, 5> &inductionVars,
+                       const llvm::DenseMap<Value, int> &operandIdxMap);
+  void moveOperationsToCurrentForBody(
+      const size_t groupIdx, OpBuilder &b,
+      const llvm::SmallVector<Value, 5> &inductionVars,
+      const llvm::DenseMap<Value, int> &operandIdxMap,
+      const ValueRange &loopState, std::queue<Operation *> &queue);
+
+  // multireduction forloop  methods
+  scf::ForOp generateMultiReductionForLoop(const size_t grpIdx);
+  scf::ForOp
+  parallelAxisGenerateForLoop(OpBuilder &opBuilder, const int groupIdx,
+                              const size_t parallelIdx, ValueRange &initArgs,
+                              llvm::SmallVector<Value, 5> &inductionVars,
+                              Value &originalWriteResult);
+
+  scf::ForOp
+  reductionAxisGenerateForLoop(OpBuilder &opBuilder, const int groupIdx,
+                               const size_t reductionIdx, ValueRange &initArgs,
+                               llvm::SmallVector<Value, 5> &inductionVars);
+
+  vector::TransferReadOp cloneReductionTransferRead(
+      Value &source, OpBuilder &b, IRMapping &readMap,
+      const llvm::SmallVector<int64_t, 4> &parallelAxis,
+      llvm::SmallVector<Value, 5> &inductionVars, bool lastDimReduction,
+      MultiReduceOpAxisKind rdKind = MultiReduceOpAxisKind::Parallel);
+};
+
+class VectorOperationAnalysizer : virtual public CanonicalizerCommonUsedData {
+private:
+  func::FuncOp func;
+
+public:
+  virtual ~VectorOperationAnalysizer(){};
+  void generateEmptyTensorAndWrite(
+      Operation *sourceOp, llvm::DenseMap<Operation *, std::pair<Value, Value>>
+                               &srcOpCanoniclizedMap);
+  void setAnalysisFunc(func::FuncOp &func) { this->func = func; }
+  void analysisEmptyGroupAndMaxSteps();
+  void analysisGroupOperaion();
+  void analysisGroupOperationResults();
+};
+
+class CanonicalizerVectorOperation : virtual public VectorOperationAnalysizer,
+                                     ForLoopGenerator {
+private:
+  func::FuncOp func;
+  IRRewriter rewriter;
+  CanonicalizerKind kind;
+
+public:
+  CanonicalizerVectorOperation(
+      func::FuncOp func,
+      CanonicalizerKind kind = CanonicalizerKind::OperationsGroup,
+      HardWareInfo hwInfo = {})
+      : func(func), rewriter(func), kind(kind) {
+    setAnalysisFunc(func);
+    setGeneratorFunc(func);
+    setHardWareInfo(hwInfo);
+    // vector operation fusion
+    if (kind == CanonicalizerKind::OperationsGroup) {
+      auto fusionStrategy = VectorFusionStrategy(func);
+      fusionStrategy.run();
+      setFuseStrategy(fusionStrategy);
+    }
+  }
+  virtual ~CanonicalizerVectorOperation(){};
+
+  // get functions
+  func::FuncOp &getFunc() { return func; };
+  IRRewriter &getIRWewriter() { return rewriter; }
+  //
+  void canonicalizeSpecialOperation();
+  LogicalResult canonicalizeReductionOperation();
+  void clearSpecialOperationCanonicalizers();
+  void dummyInitSpecialOperation();
+  void initSpeicalOperationCanonicalizers();
+
+  void run();
+};
+} // namespace
+} // namespace vector
+} // namespace mlir
+#endif
---
+++ mlir/test/Dialect/Vector/vector-tiling.mlir
@@ -0,0 +1,143 @@
+// RUN: mlir-opt %s --split-input-file --lower-to-tile-vector --tiling-vector-pass | FileCheck %s
+
+// CHECK-LABEL: func @add_tensor_test0
+func.func @add_tensor_test0(%arg0: tensor<11008x4096xf32>, %arg1: tensor<11008x4096xf32>) -> tensor<11008x4096xf32> {
+  // CHECK: %[[C4096:.*]] = arith.constant 4096 : index
+  // CHECK: %[[C16:.*]] = arith.constant 16 : index
+  // CHECK: %[[C11008:.*]] = arith.constant 11008 : index
+  // CHECK: %[[C1:.*]] = arith.constant 1 : index
+  // CHECK: %[[C0:.*]] = arith.constant 0 : index
+  // CHECK: %[[CST:.*]] = arith.constant 0.000000e+00 : f32
+  // CHECK: %[[TENSOR0:.*]] = tensor.empty() : tensor<11008x4096xf32>
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: %[[READ0:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<11008x4096xf32>, vector<16xf32>
+  // CHECK: %[[READ1:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<11008x4096xf32>, vector<16xf32>
+  // CHECK: %[[ADD0:.*]] = arith.addf %[[READ0]], %[[READ1]] : vector<16xf32>
+  // CHECK: %[[ADD1:.*]] = arith.addf %[[ADD0]], %[[READ1]] : vector<16xf32>
+  // CHECK: %[[WRITE:.*]] = vector.transfer_write {{.*}}, {{.*}} : vector<16xf32>, tensor<11008x4096xf32>
+  %0 = tensor.empty() : tensor<11008x4096xf32>
+  %1 = linalg.add ins(%arg0, %arg1 : tensor<11008x4096xf32>, tensor<11008x4096xf32>) outs(%0: tensor<11008x4096xf32>) -> tensor<11008x4096xf32>
+  %2 = linalg.add ins(%1, %arg1 : tensor<11008x4096xf32>, tensor<11008x4096xf32>) outs(%0: tensor<11008x4096xf32>) -> tensor<11008x4096xf32>
+  return %2 : tensor<11008x4096xf32>
+}
+
+// CHECK-LABEL: func @fc_relu
+func.func @fc_relu(%lhs: tensor<512x512xf32>, %rhs: tensor<512x512xf32>,
+                   %bias: tensor<512x512xf32>, %output: tensor<512x512xf32>)
+                   -> tensor<512x512xf32> {
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: %[[READ0:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<512x512x512xf32>, vector<16xf32>
+  // CHECK: %[[READ1:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<512x512x512xf32>, vector<16xf32>
+  // CHECK: %[[MULF0:.*]] = arith.mulf %[[READ0]], %[[READ1]] : vector<16xf32>
+  // CHECK: %[[WRITE:.*]] = vector.transfer_write {{.*}}, {{.*}} : vector<16xf32>, tensor<512x512x512xf32>
+  // CHECK-DAG: vector.multi_reduction
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: %[[READ0:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<512x512xf32>, vector<16xf32>
+  // CHECK: %[[READ1:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<512x512xf32>, vector<16xf32>
+  // CHECK:  %[[ADD0:.*]] = arith.addf %[[READ1]], %[[READ0]] : vector<16xf32>
+  // CHECK:  %[[ADD1:.*]] = arith.maximumf %[[ADD0]], {{.*}} : vector<16xf32>
+  // CHECK: %[[WRITE:.*]] = vector.transfer_write {{.*}}, {{.*}} : vector<16xf32>, tensor<512x512xf32>
+  // Matrix-matrix multiplication.
+  %matmul = linalg.matmul ins(%lhs, %rhs: tensor<512x512xf32>, tensor<512x512xf32>)
+                          outs(%output: tensor<512x512xf32>) -> tensor<512x512xf32>
+
+  // Elementwise addition.
+  %biased = linalg.elemwise_binary { fun = #linalg.binary_fn<add> }
+    ins(%matmul, %bias : tensor<512x512xf32>, tensor<512x512xf32>)
+    outs(%output : tensor<512x512xf32>) -> tensor<512x512xf32>
+
+  // Elementwise max with 0 (ReLU).
+  %c0f = arith.constant 0.0 : f32
+  // expected-remark @below {{elementwise binary}}
+  %relued = linalg.elemwise_binary { fun = #linalg.binary_fn<max_signed> }
+    ins(%biased, %c0f : tensor<512x512xf32>, f32)
+    outs(%output : tensor<512x512xf32>) -> tensor<512x512xf32>
+  func.return %relued : tensor<512x512xf32>
+}
+
+// CHECK-LABEL: func @matmul_add
+func.func @matmul_add(%arg0: tensor<8192x12288xf16>, %arg1: tensor<12288x16384xf16>, %arg2: tensor<8192x16384xf32>, %arg3: tensor<8192x16384xf32>) -> tensor<8192x16384xf32> {
+  // CHECK: vector.broadcast
+  // CHECK: vector.transpose
+  // CHECK: vector.broadcast
+  // CHECK: vector.transpose
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: %[[READ0:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<32x32x12288xf16>, vector<16xf16>
+  // CHECK: %[[EXTF0:.*]] = arith.extf %[[READ0]] : vector<16xf16> to vector<16xf32>
+  // CHECK: %[[READ1:.*]] = vector.transfer_read {{.*}}, {{.*}} : tensor<32x32x12288xf16>, vector<16xf16>
+  // CHECK: %[[EXTF1:.*]] = arith.extf %[[READ1]] : vector<16xf16> to vector<16xf32>
+  // CHECK: %[[MULF0:.*]] = arith.mulf %[[EXTF0]], %[[EXTF1]] : vector<16xf32>
+  // CHECK: %[[WRITE0:.*]] = vector.transfer_write {{.*}}, {{.*}} {in_bounds = [true]} : vector<16xf32>, tensor<32x32x12288xf32>
+  // CHECK-DAG: vector.multi_reduction
+  // CHECK: scf.for
+  // CHECK: scf.for
+  // CHECK: %[[READ2:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<8192x16384xf32>, vector<16xf32>
+  // CHECK: %[[READ3:.*]] = vector.transfer_read {{.*}}, {{.*}}: tensor<32x32xf32>, vector<16xf32>
+  // CHECK: %[[ADD0:.*]] = arith.addf %[[READ3]], %[[READ2]] : vector<16xf32>
+  // CHECK: %[[WRITE1:.*]] = vector.transfer_write {{.*}}, {{.*}} : vector<16xf32>, tensor<128x128xf32>
+  %0 = linalg.matmul {__fused_op__ = [0], name = "dot", tile_sizes = [[128, 128], [32, 32]]} ins(%arg0, %arg1 : tensor<8192x12288xf16>, tensor<12288x16384xf16>) outs(%arg2 : tensor<8192x16384xf32>) -> tensor<8192x16384xf32>
+  %1 = tensor.empty() : tensor<8192x16384xf32>
+  %2 = linalg.generic {indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>], iterator_types = ["parallel", "parallel"]} ins(%0, %arg3 : tensor<8192x16384xf32>, tensor<8192x16384xf32>) outs(%1 : tensor<8192x16384xf32>) attrs =  {__root_op__ = 0 : i64} {
+  ^bb0(%in: f32, %in_0: f32, %out: f32):
+    %4 = arith.addf %in, %in_0 : f32
+    linalg.yield %4 : f32
+  } -> tensor<8192x16384xf32>
+  %c0 = arith.constant 0 : index
+  %c8192 = arith.constant 8192 : index
+  %c128 = arith.constant 128 : index
+  %3 = scf.for %arg4 = %c0 to %c8192 step %c128 iter_args(%arg5 = %1) -> (tensor<8192x16384xf32>) {
+    %c0_0 = arith.constant 0 : index
+    %c16384 = arith.constant 16384 : index
+    %c128_1 = arith.constant 128 : index
+    %4 = scf.for %arg6 = %c0_0 to %c16384 step %c128_1 iter_args(%arg7 = %arg5) -> (tensor<8192x16384xf32>) {
+      %extracted_slice = tensor.extract_slice %arg0[%arg4, 0] [128, 12288] [1, 1] : tensor<8192x12288xf16> to tensor<128x12288xf16>
+      %extracted_slice_2 = tensor.extract_slice %arg1[0, %arg6] [12288, 128] [1, 1] : tensor<12288x16384xf16> to tensor<12288x128xf16>
+      %extracted_slice_3 = tensor.extract_slice %arg2[%arg4, %arg6] [128, 128] [1, 1] : tensor<8192x16384xf32> to tensor<128x128xf32>
+      %5 = linalg.matmul {__fused_op__ = [0], name = "dot", tile_sizes = [[128, 128], [32, 32]]} ins(%extracted_slice, %extracted_slice_2 : tensor<128x12288xf16>, tensor<12288x128xf16>) outs(%extracted_slice_3 : tensor<128x128xf32>) -> tensor<128x128xf32>
+      %extracted_slice_4 = tensor.extract_slice %0[%arg4, %arg6] [128, 128] [1, 1] : tensor<8192x16384xf32> to tensor<128x128xf32>
+      %extracted_slice_5 = tensor.extract_slice %arg3[%arg4, %arg6] [128, 128] [1, 1] : tensor<8192x16384xf32> to tensor<128x128xf32>
+      %extracted_slice_6 = tensor.extract_slice %arg7[%arg4, %arg6] [128, 128] [1, 1] : tensor<8192x16384xf32> to tensor<128x128xf32>
+      %6 = linalg.generic {indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>], iterator_types = ["parallel", "parallel"]} ins(%5, %extracted_slice_5 : tensor<128x128xf32>, tensor<128x128xf32>) outs(%extracted_slice_6 : tensor<128x128xf32>) attrs =  {__root_op__ = 0 : i64} {
+      ^bb0(%in: f32, %in_9: f32, %out: f32):
+        %8 = arith.addf %in, %in_9 : f32
+        linalg.yield %8 : f32
+      } -> tensor<128x128xf32>
+      %c0_7 = arith.constant 0 : index
+      %c128_8 = arith.constant 128 : index
+      %c32 = arith.constant 32 : index
+      %7 = scf.for %arg8 = %c0_7 to %c128_8 step %c32 iter_args(%arg9 = %extracted_slice_6) -> (tensor<128x128xf32>) {
+        %c0_9 = arith.constant 0 : index
+        %c128_10 = arith.constant 128 : index
+        %c32_11 = arith.constant 32 : index
+        %8 = scf.for %arg10 = %c0_9 to %c128_10 step %c32_11 iter_args(%arg11 = %arg9) -> (tensor<128x128xf32>) {
+          %extracted_slice_12 = tensor.extract_slice %extracted_slice[%arg8, 0] [32, 12288] [1, 1] : tensor<128x12288xf16> to tensor<32x12288xf16>
+          %extracted_slice_13 = tensor.extract_slice %extracted_slice_2[0, %arg10] [12288, 32] [1, 1] : tensor<12288x128xf16> to tensor<12288x32xf16>
+          %extracted_slice_14 = tensor.extract_slice %extracted_slice_3[%arg8, %arg10] [32, 32] [1, 1] : tensor<128x128xf32> to tensor<32x32xf32>
+          %9 = linalg.matmul {__fused_op__ = [0], name = "dot", tile_sizes = [[128, 128], [32, 32]]} ins(%extracted_slice_12, %extracted_slice_13 : tensor<32x12288xf16>, tensor<12288x32xf16>) outs(%extracted_slice_14 : tensor<32x32xf32>) -> tensor<32x32xf32>
+          %extracted_slice_15 = tensor.extract_slice %5[%arg8, %arg10] [32, 32] [1, 1] : tensor<128x128xf32> to tensor<32x32xf32>
+          %extracted_slice_16 = tensor.extract_slice %extracted_slice_5[%arg8, %arg10] [32, 32] [1, 1] : tensor<128x128xf32> to tensor<32x32xf32>
+          %extracted_slice_17 = tensor.extract_slice %arg11[%arg8, %arg10] [32, 32] [1, 1] : tensor<128x128xf32> to tensor<32x32xf32>
+          %10 = linalg.generic {indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>], iterator_types = ["parallel", "parallel"]} ins(%9, %extracted_slice_16 : tensor<32x32xf32>, tensor<32x32xf32>) outs(%extracted_slice_17 : tensor<32x32xf32>) attrs =  {__root_op__ = 0 : i64} {
+          ^bb0(%in: f32, %in_19: f32, %out: f32):
+            %11 = arith.addf %in, %in_19 : f32
+            linalg.yield %11 : f32
+          } -> tensor<32x32xf32>
+          %inserted_slice_18 = tensor.insert_slice %10 into %arg11[%arg8, %arg10] [32, 32] [1, 1] : tensor<32x32xf32> into tensor<128x128xf32>
+          scf.yield %inserted_slice_18 : tensor<128x128xf32>
+        } {__parallel_loop__ = 1 : i64}
+        scf.yield %8 : tensor<128x128xf32>
+      } {__parallel_loop__ = 1 : i64}
+      %inserted_slice = tensor.insert_slice %7 into %arg7[%arg4, %arg6] [128, 128] [1, 1] : tensor<128x128xf32> into tensor<8192x16384xf32>
+      scf.yield %inserted_slice : tensor<8192x16384xf32>
+    } {__parallel_loop__ = 0 : i64}
+    scf.yield %4 : tensor<8192x16384xf32>
+  } {__parallel_loop__ = 0 : i64}
+  return %3 : tensor<8192x16384xf32>
+}
+
